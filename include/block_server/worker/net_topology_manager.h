2d445a239f80f9799e107d9ccb0f1d8c72b75f9b
XQ1203 <2292139705@qq.com>
2023-05-14 22:32:37 +0800

    tmp

---
+++ include/block_server/worker/net_topology_manager.h
@@ -0,0 +1,14 @@
+//
+// Created by user on 23-5-13.
+//
+
+#ifndef NEU_BLOCKCHAIN_NET_TOPOLOGY_MANAGER_H
+#define NEU_BLOCKCHAIN_NET_TOPOLOGY_MANAGER_H
+
+#endif //NEU_BLOCKCHAIN_NET_TOPOLOGY_MANAGER_H
+
+class NetTopologyManager{
+public:
+    ~NetTopologyManager() = default;
+    void NodeConnectionClog();
+};
---
+++ src/block_server/worker/net_topology_manager.cpp
@@ -0,0 +1,9 @@
+//
+// Created by user on 23-5-13.
+//
+#include "block_server/worker/net_topology_manager.h"
+
+void NetTopologyManager::NodeConnectionClog() {
+
+}
+
